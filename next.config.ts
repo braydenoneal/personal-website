db3699509ee808a99278d8503e9bdf45c7827a3e
Brayden O'Neal <braydenoneal@protonmail.com>
2025-06-08 13:41:49 -0500

    Setup firebase

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     distDir: 'build',
+    output: 'export',
+    images: { unoptimized: true },
 }
 
 export default nextConfig
